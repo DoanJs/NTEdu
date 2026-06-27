ebd5702cdc19cef5e5bc22092b72a2b53a910147
DoanJs <viethungtrieuphong1994@gmail.com>
2026-06-27 15:07:28 +0700

    fix error in IPhone

--- src/screens/admin/AdminChildren.tsx
+++ src/screens/admin/AdminChildren.tsx
@@ -232,9 +232,10 @@ export default function ChildrenScreen() {
       } else if (err.code === "functions/not-found") {
         handleToastError("Không tìm thấy trẻ");
       } else {
-        handleToastError(
-          childEdit ? "Cập nhật trẻ thất bại" : "Thêm trẻ mới thất bại",
-        );
+        handleToastError(`ERROR: ${err} - CODE: ${err.code} - MESSAGE: ${err.message}`);
+        // handleToastError(
+        //   childEdit ? "Cập nhật trẻ thất bại" : "Thêm trẻ mới thất bại",
+        // );
       }
     } finally {
       setIsLoading(false);
